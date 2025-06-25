24860748836b2e3ee1a12889376aad71fd2c5696
maxbader <mibader@uci.edu>
2025-06-25 16:30:33 -0700

    set up basic file structure under src. created load_data function to load csv sleep data into a DataFrame.

---
+++ src/data.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from pathlib import Path
+
+# stores sleep history data
+DATA_FILE = Path(__file__).parent.parent / "sleep_data.csv"
+
+def load_data() -> pd.DataFrame:
+    """
+    Loads the sleep data CSV into a DataFrame.
+    If the file doesn't exist yet, we should return empty DataFrame
+    with columns: date, alarm_time, wakeup_time, snoozes.
+    """
+    # desired schema
+    cols = ["date", "alarm_time", "wakeup_time", "snoozes"]
+
+    if not DATA_FILE.exists():
+        return pd.DataFrame(columns=cols)
+    
+    # read the csv; parsing 'date' columnn as datetime
+    df = pd.read_csv(
+        DATA_FILE,
+        parse_dates=["date"],
+        usecols=cols,           # gaurds against extra columns
+    )
+
+    return df
+
+
+def save_entry(entry: dict) ->  None:
+    """
+    Append a single entry (with keys, date, alarm_time, wakeup_time, snoozes)
+    into CSV. Create file if doesn't exist.
+    """
